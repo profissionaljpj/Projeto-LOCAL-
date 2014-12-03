1e2ca7e383b3589f8e25dca8da0b5d6147240abc
WickedPeanuts <carlos_judo@hotmail.com>
2014-12-03 20:58:43 -0300

    Inserção PDF

---
+++ Banco de Dados (Scripts)/SQLQuery1.sql
@@ -0,0 +1,26 @@
+create database PocketQueue;
+
+use PocketQueue;
+
+
+create table Usuario(
+	IDUsuario int identity primary key,
+	Nome varchar(50),
+	Cpf varchar(20),
+	ULogin varchar(20),
+	USenha varchar(20),
+	TipoUsuario varchar(20),
+);
+
+create table Senha(
+	IDSenha int identity primary key,
+	Criacao Datetime DEFAULT ('12/30/1899 23:59:59'),
+	Atendido Datetime DEFAULT ('12/30/1899 23:59:59'),
+	Resolvido Datetime DEFAULT ('12/30/1899 23:59:59'),
+	Guiche int,
+	TipoSenha varchar(20),
+	EstadoSenha varchar(20),
+	IDUsuario int foreign key references Usuario(IDUsuario),
+);
+
+insert into Usuario (Nome, Cpf, USogin, USenha, TipoUsuario) values ('Gerente', '123', 'Gerente', '123', 'Gerente');
